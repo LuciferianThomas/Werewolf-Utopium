fa0d3fa839e9f53626f7303dead3db2ce0c50272
Glitch (hello-sqlite) <none>
2019-07-07 00:20:54 +0000

    🛋💇 Checkpoint ./index.js:4959043/1191 ./watch.json:4959043/2

--- index.js
+++ index.js
@@ -19,17 +19,17 @@ const client = new Discord.Client(),
       userData = new db.table("USERDATA"),
       guildData = new db.table("GUILDDATA")
 
-let date = (date = moment()) => {
+let date = async (date = moment()) => {
   return moment(date).format("D MMM Y HH:mm [GMT]")
 }
 
-let send = (content, config) => {
+let send = async (content, config) => {
   let { client, message } = config
   if (content instanceof Discord.RichEmbed) {
     message.channel.send(content).catch(e => {
       message.author.send(content).then(message.author.send("*I need the `Embed Links` permission!*"))
         .catch(er => {
-          message.channel.send("I need the `Embed Links` permission!")
+          message.channel.send("I need the `Embed Links` permission!").catch(console.error)
         })
     })
   } else if (content instanceof Object) {
@@ -42,9 +42,26 @@ let send = (content, config) => {
       .setTimestamp()
     
     message.channel.send(embed).catch(e => {
-      message.channel.send(`**${title}**\n${description}\n\n`)
+      message.channel.send(`**${title}**\n${description}\n\n*I need the \`Embed Links\` permission!`).catch(er => {
+        message.author.send(embed).then(message.author.send("*I need the `Send Messages` and `Embed Links` permissions!*")).catch(console.error)
+      })
     })
+  } else if (typeof content == "string") {
+    let embed = new Discord.RichEmbed()
+      .setColor(embedColor)
+      .setTitle(content)
+      .setFooter(client.user.username, client.user.avatarURL)
+      .setTimestamp()
+    
+    message.channel.send(embed).catch(e => {
+      message.channel.send(`${content}\n\n*I need the \`Embed Links\` permission!`).catch(er => {
+        message.author.send(embed).then(message.author.send("*I need the `Send Messages` and `Embed Links` permissions!*")).catch(console.error)
+      })
+    })
+  } else {
+    // Error('Invalid output type.\nAccepts Discord.RichEmbed, Object or String.')
   }
+  return undefined
 }
 
 /* --- ALL GLOBAL CONSTANTS & FUNCTIONS --- */
@@ -100,7 +117,7 @@ client.on('guildCreate', async guild => {
 // for guilds
 client.on('message', async message => {
   
-  if (message.author.bot || msg.channel.type != 'text') return;
+  if (message.author.bot || message.channel.type != 'text') return;
   
   console.log(`${message.guild.name} #${message.channel.name} | ${message.author.tag} > ${message.cleanContent}`)
   
