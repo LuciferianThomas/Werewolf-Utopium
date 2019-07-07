29ad56746396d2dd79c590c411a78314a43708de
Glitch (hello-sqlite) <none>
2019-07-07 00:09:55 +0000

    💳🚤 Checkpoint ./index.js:4959043/3065

--- index.js
+++ index.js
@@ -27,7 +27,22 @@ let send = (content, config) => {
   let { client, message } = config
   if (content instanceof Discord.RichEmbed) {
     message.channel.send(content).catch(e => {
-      message.author.send(content).catch()
+      message.author.send(content).then(message.author.send("*I need the `Embed Links` permission!*"))
+        .catch(er => {
+          message.channel.send("I need the `Embed Links` permission!")
+        })
+    })
+  } else if (content instanceof Object) {
+    let { title, description } = content
+    let embed = new Discord.RichEmbed()
+      .setColor(embedColor)
+      .setTitle(title)
+      .setDescription(description)
+      .setFooter(client.user.username, client.user.avatarURL)
+      .setTimestamp()
+    
+    message.channel.send(embed).catch(e => {
+      message.channel.send(`**${title}**\n${description}\n\n`)
     })
   }
 }
