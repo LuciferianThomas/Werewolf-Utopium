b19a2c53b673d8891e0188549c83b49070e7ac35
Glitch (hello-sqlite) <none>
2019-07-07 08:23:09 +0000

    🌞💸 Checkpoint ./index.js:4959043/1810

--- index.js
+++ index.js
@@ -16,52 +16,8 @@ const Discord = require('discord.js'),
 const client = new Discord.Client(),
       { defaultPrefix, embedColor } = require('./util/config.js'),
       userData = new db.table("USERDATA"),
-      guildData = new db.table("GUILDDATA")
-
-let date = async (date = moment()) => {
-  return moment(date).format("D MMM Y HH:mm [GMT]")
-}
-
-let send = async (content, config) => {
-  let { client, message } = config
-  if (content instanceof Discord.RichEmbed) {
-    message.channel.send(content).catch(e => {
-      message.author.send(content).then(message.author.send("*I need the `Embed Links` permission!*"))
-        .catch(er => {
-          message.channel.send("I need the `Embed Links` permission!").catch(console.error)
-        })
-    })
-  } else if (content instanceof Object) {
-    let { title, description } = content
-    let embed = new Discord.RichEmbed()
-      .setColor(embedColor)
-      .setTitle(title)
-      .setDescription(description)
-      .setFooter(client.user.username, client.user.avatarURL)
-      .setTimestamp()
-    
-    message.channel.send(embed).catch(e => {
-      message.channel.send(`**${title}**\n${description}\n\n*I need the \`Embed Links\` permission!`).catch(er => {
-        message.author.send(embed).then(message.author.send("*I need the `Send Messages` and `Embed Links` permissions!*")).catch(console.error)
-      })
-    })
-  } else if (typeof content == "string") {
-    let embed = new Discord.RichEmbed()
-      .setColor(embedColor)
-      .setTitle(content)
-      .setFooter(client.user.username, client.user.avatarURL)
-      .setTimestamp()
-    
-    message.channel.send(embed).catch(e => {
-      message.channel.send(`${content}\n\n*I need the \`Embed Links\` permission!`).catch(er => {
-        message.author.send(embed).then(message.author.send("*I need the `Send Messages` and `Embed Links` permissions!*")).catch(console.error)
-      })
-    })
-  } else {
-    // Error('Invalid output type.\nAccepts Discord.RichEmbed, Object or String.')
-  }
-  return undefined
-}
+      guildData = new db.table("GUILDDATA"),
+      fn = require('./util')
 
 /* --- ALL GLOBAL CONSTANTS & FUNCTIONS --- */
 
