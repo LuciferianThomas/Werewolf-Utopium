0221e31c6bef2a0ef0c764cc4bca84b365a93fc5
Glitch (hello-sqlite) <none>
2019-07-08 09:09:42 +0000

    🏌⛳️ Checkpoint ./bot/tempmute.js:4959043/1297

--- bot/tempmute.js
+++ bot/tempmute.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const db = require('quick.db')
+const moment = require('moment')
 
 const guildData = new db.table("GUILDDATA"),
       config = require('./config.js'),
@@ -8,7 +9,16 @@ const guildData = new db.table("GUILDDATA"),
 module.exports = (client) => {
   client.on('ready', () => {
     setInterval(() => {
-      let tempmutes = guildData.all().map()
+      let guilds = guildData.all()
+        .filter(guild => JSON.parse(guild.data).tempmutes)
+      for (var i = 0; i < guilds.length; i++) {
+        let guildTMs = JSON.parse(guilds[i].data).tempmutes
+        for (var j = 0; j < guildTMs.length; j++) {
+          if (moment(guildTMs[j].unmute) > moment()) {
+            let muteRole = client.guilds.getguildData.get(`${guilds[i]}.muteRole`)
+          }
+        }
+      }
     }, 5*1000)
   })
 }
