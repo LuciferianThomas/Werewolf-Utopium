5765eea6e5d588b36abf511d11409676f8b005ca
Glitch (hello-sqlite) <none>
2019-07-08 07:19:52 +0000

    🚵🐺 Checkpoint ./commands/tempmute.js:4959043/93 ./bot/tempmute.js:4959043/27

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -5,8 +5,7 @@ const config = require('/app/bot/config.js'),
       fn = require('/app/bot/fn.js')
 const userData = new db.table("USERDATA"),
       guildData = new db.table("GUILDDATA"),
-      modCases = new db.table("MODCASES"),
-      tempmutes = new db.table("TEMPMUTES")
+      modCases = new db.table("MODCASES")
 
 module.exports = {
 	name: "mute",
