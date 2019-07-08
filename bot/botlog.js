35fd68d219fcc8f1c7789a57102e4f0adbdd96e5
Glitch (hello-sqlite) <none>
2019-07-08 13:21:04 +0000

    🌷🎯 Checkpoint ./bot/botlog.js:4959043/1983

--- bot/botlog.js
+++ bot/botlog.js
@@ -22,6 +22,47 @@ module.exports = (client) => {
       .addField("Tag", member.user.tag, true)
       .addField("ID", member.id, true)
       .addField("Joined at", fn.date(member.joinedAt))
+      .setFooter(client.user.username, client.user.avatarURL)
+      .setTimestamp()
+    
+    log.send(embed)
+  })
+  
+  client.on('guildMemberRemove', member => {
+    let log = guildData.get(`${member.guild.id}.botlog`)
+    if (!log) return;
+    
+    let embed = new Discord.RichEmbed()
+      .setColor(config.embedColor)
+      .setTitle("Member Left")
+      .setThumbnail(member.user.displayAvatarURL)
+      .addField("Member", `${member}`, true)
+      .addField("Tag", member.user.tag, true)
+      .addField("ID", member.id, true)
+      .addField("Joined at", fn.date(member.joinedAt))
+      .setFooter(client.user.username, client.user.avatarURL)
+      .setTimestamp()
+    
+    log.send(embed)
+  })
+  
+  client.on('messageUpdate', (oldMessage, newMessage) => {
+    if (newMessage.channel.type != 'text') return;
+    
+    let log = guildData.get(`${newMessage.guild.id}.botlog`)
+    if (!log) return;
+    
+    let embed = new Discord.RichEmbed()
+      .setColor(config.embedColor)
+      .setTitle("Message Edited")
+      .addField("Before", oldMessage.content)
+      .addField("After", newMessage.content)
+      .addField("Sent by", `${newMessage.author}`, true)
+      .addField("In channel", fn.date(member.joinedAt))
+      .setFooter(client.user.username, client.user.avatarURL)
+      .setTimestamp()
+    
+    log.send(embed)
   })
   
 }
