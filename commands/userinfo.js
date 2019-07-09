3c8a998d14611c63deeb52133a08e6e8e5ba4f35
Glitch (hello-sqlite) <none>
2019-07-09 02:58:10 +0000

    💐🎠 Checkpoint ./commands/userinfo.js:4959043/778

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -10,6 +10,7 @@ const userData = new db.table("USERDATA"),
       modCases = new db.table("MODCASES")
 
 let activities = ['Playing', 'Streaming', 'Listening', 'Watching']
+let statuses = {online: "Online", idle: "Idle", dnd: "DND", offline: "Offline"}
 
 module.exports = {
   name: "userinfo",
@@ -18,22 +19,23 @@ module.exports = {
   category: "Utility",
   run: async (client, message, args, shared) => {
     let target = message.member
-    if (args[0]) target = fn.getUser(client, args[0])
-    if (message.mentions.members.filter(member => member.id != client.user.id).size) target = message.mentions.members.filter(member => member.id != client.user.id).first()
+    if (args[0]) target = fn.getMember(message.guild, args[0])
+    if (message.mentions.members.first().user.id == client.user.id) target = message.mentions.members.filter(member => member.id != client.user.id).first()
     
-    let user = userData.get()
+    if (userData.has(target.user.id)) var user = userData.get(target.user.id)
+    else var user = {botStaff: false, blacklisted: false, commandsUsed: 0}
     
     let embed = new Discord.RichEmbed()
       .setColor(config.embedColor)
       .setTitle(`${target.user.tag} | Information`)
       .setThumbnail(target.user.displayAvatarURL)
-      .addField(target.bot ? "Bot" : "User", `${target}`, true)
+      .addField(target.user.bot ? "Bot" : "User", `${target}`, true)
       .addField("Joined Discord", fn.date(target.user.createdAt), true)
-      .addField('Current Status', `${target.user.presence.status.toUpperCase()}`, true)
+      .addField('Current Status', `${statuses[target.user.presence.status]}`, true)
       .addField('Current Activity', target.user.presence.game ? `${activities[target.user.presence.game.type]} ${target.user.presence.game.name}` : "None", true)
-      .addField(`${client.user.username} Tags`, shared.user.botStaff || shared.user.blacklisted ? (shared.user.botStaff ? "Bot Staff" : "" + '\n' + shared.user.blacklisted ? "Blacklsited" : "") : "None", true)
-      .addField(`${client.user.username} Commands Used`, shared.user.commandsUsed, true)
-      .addField(`Roles [${target.roles.size-1}]`, target.roles.map(r => `${r}`).slice(1).join(' '), true)
+      .addField(`${client.user.username} Tags`, (user.botStaff || user.blacklisted) ? (user.botStaff ? "Bot Staff" : "" + '\n' + user.blacklisted ? "Blacklisted" : "") : "None", true)
+      .addField(`${client.user.username} Commands Used`, user.commandsUsed, true)
+    if (target.roles.size > 1) embed.addField(`Role${target.roles.size == 2 ? "" : "s"} [${target.roles.size-1}]`, target.roles.map(r => `${r}`).slice(1).join(' '), true)
     
     message.channel.send(embed)
   }
