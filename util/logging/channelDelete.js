ac328feabc6c29b4051042b04e6974fac3219b2d
Glitch (project-unity) <none>
2019-07-23 07:20:11 +0000

    🎩📷 Checkpoint ./util/logging/channelCreate.js:4959043/1728 ./util/logging/channelDelete.js:4959043/1074

---
+++ util/logging/channelDelete.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js'),
+      db = require('quick.db')
+
+const guildData = new db.table("GUILDDATA")
+
+const config = require('/app/bot/config.js'),
+      fn = require('/app/bot/fn.js')
+
+module.exports = (client) => {
+  client.on('channelDelete', async channel => {
+    if (!channel.guild) return;
+    let logChannelID = guildData.get(`${channel.guild.id}.botlog`)
+    let logChannel = client.channels.get(logChannelID)
+    if (!logChannel) return;
+    
+    logChannel.send(
+      new Discord.RichEmbed()
+        .setColor(config.embedColor)
+        .setAuthor("Channel Deleted")
+        .setIcon(channel.guild.iconURL)
+        .addField((channel.type == "text" || channel.type == "news" || channel.type == "store") ? "Text Channel" : channel.type == 'voice' ? "Voice Channel" : "Category", `${channel} (${channel.name})`, true)
+        .addField("ID", channel.id, true)
+        .addField("Created", fn.date(channel.createdTimestamp))
+        .setFooter(client.user.username, client.user.avatarURL)
+        .setTimestamp()
+    )
+  })
+}
