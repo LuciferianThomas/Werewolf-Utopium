a69a16b0389a40e856e5718fc2b33517640d8d47
Glitch (project-unity) <none>
2019-07-25 04:06:08 +0000

    👂💴 Checkpoint ./index.js:4959043/50

--- index.js
+++ index.js
@@ -23,14 +23,13 @@ const app = express()
 app.use(express.static('public'));
 
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-  console.log(`${fn.time()} | Ping Received`);
+  response.sendFile(__dirname + '/views/index.html')
 });
 
 const listener = app.listen(process.env.PORT, function() {
   setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-  }, 225000);
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`)
+  }, 225000)
 });
 
 client.commands = new Discord.Collection()
