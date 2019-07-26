ea0c70d5cffa66a6f0b762a0132dba540ce0c58f
Glitch (project-unity) <none>
2019-07-26 08:31:35 +0000

    🎄🙍 Checkpoint ./commands/guildinfo.js:4959043/3630 ./watch.json:4959043/2

--- commands/guildinfo.js
+++ commands/guildinfo.js
@@ -16,15 +16,19 @@ module.exports = {
   category: "Utility",
   guildPerms: ["SEND_MESSAGES"],
   run: async (client, message, args, shared) => {
+    let guild = message.guild
+    
     let embed = new Discord.RichEmbed()
       .setColor(config.embedColor)
-      .setTitle(`${message.guild} | Information`)
-      .setThumbnail(message.guild.iconURL)
-      .addField("Name", `${message.guild}`, true)
-      .addField("Created", fn.date(message.guild.createdAt), true)
-      .addField(`Member${message.guild.members.size == 1 ? "" : "s"} [${message.guild.members.size}]`, `${message.guild.members.size}\n${message.guild.members.filter(member => !member.user.bot).size} Humans (${message.guild.members.filter(member => !member.user.bot && member.user.presence.status == ).size})\n${message.guild.members.filter(member => member.user.bot).size} Bots`, true)
-      .addField('Current Activity', target.user.presence.game ? `${activities[target.user.presence.game.type]} ${target.user.presence.game.name}` : "None", true)
-      .addField(`${client.user.username} Tags`, (user.botStaff || user.blacklisted) ? (user.botStaff ? "Bot Staff" : "" + '\n' + user.blacklisted ? "Blacklisted" : "") : "None", true)
+      .setTitle(`${guild} | Information`)
+      .setThumbnail(guild.iconURL)
+      .addField("Name", `${guild}`, true)
+      .addField("Owner", `${guild.owner}`, true)
+      .addField("Created", fn.date(guild.createdAt), true)
+      .addField("Joined", fn.date(guild.joinedAt), true)
+      .addField(`Member${guild.members.size == 1 ? "" : "s"} [${guild.members.size}]`, `${guild.members.filter(member => !member.user.bot).size} Humans (${guild.presences.filter(presence => presence.status != 'offline').size} Online)\n${guild.members.filter(member => member.user.bot).size} Bots`, true)
+      .addField(`Channel${guild.channels.size == 1 ? "" : "s"} [${guild.channels.size}]`, `${guild.channels.filter(channel => channel.type == 'text' || channel.type == 'news' || channel.type == 'store').size} Text Channels\n${guild.channels.filter(channel => channel.type == 'voice').size} Voice Channels\n${guild.channels.filter(channel => channel.type == 'category').size} Categories`, true)
+      .addField(`Role{guild.channels.size - 1 == 1 ? "" : "s"}`, `${guild.channels.size - 1}]`, true)
       .addField(`${client.user.username} Commands Used`, user.commandsUsed, true)
     if (target.roles.size > 1) embed.addField(`Role${target.roles.size == 2 ? "" : "s"} [${target.roles.size-1}]`, target.roles.map(r => `${r}`).slice(1).join(' '), true)
     
