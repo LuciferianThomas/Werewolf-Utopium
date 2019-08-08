60acd7bd1a67d65ab3f53201a57adb43d57022b0
Glitch (project-unity) <none>
2019-08-08 11:16:07 +0000

    🎪🍵 Checkpoint ./commands/infractions.js:4959043/1165

--- commands/infractions.js
+++ commands/infractions.js
@@ -12,22 +12,15 @@ module.exports = {
   aliases: ["warns", "warnings"],
   category: "Moderation",
   run: async (client, message, args, shared) => {
-    let cases = modCases.get(message.guild.id)
+    let target = message.member
+    if (args[0]) target = fn.getMember(message.guild, args[0])
+    if ([`<@${client.user.id}> `,`<@!${client.user.id}> `].includes(shared.prefix) && message.mentions.members.first().user.id == client.user.id) target = message.mentions.members.first(2)[1]
+    else if (message.mentions.members.size) target = message.mentions.members.first()
     
-    if (!cases || cases.length == 0) {
-      message.channel.send(fn.embed(client, "There are no cases yet!"))
-      modCases.set(message.guild.id, [])
-      return undefined
-    }
+    let cases = modCases.get(message.guild.id).filter(thisCase => thisCase.user == target.user.id && !thisCase.type.includes("UN"))
     
-    if (!args[0] || isNaN(parseInt(args[0]))) return message.channel.send(fn.embed(client, `There has been ${cases.length} cases!`))
+    if (!cases || cases.length == 0) return await message.channel.send(fn.embed(client, `${target} has no infractions yet!`))
     
-    var thisCase = cases.find(r => r.id == parseInt(args[0]))
-    if (!thisCase) return message.channel.send(fn.embed(client, `Case #${args[0]} does not exist!`))
     
-    let embed = fn.modCaseEmbed(client, thisCase)
-    
-    message.channel.send(embed)
-    return undefined
   }
 }
