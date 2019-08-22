d35f123c5c0cc27ebb23ec9852accadfebc3a4ae
Glitch (project-unity) <none>
2019-08-22 06:56:20 +0000

    🐑🐢 Checkpoint ./bot/config.js:4959043/19

--- bot/config.js
+++ bot/config.js
@@ -2,5 +2,6 @@ module.exports = {
   defaultPrefix: "u!",
   embedColor: 0x708ad7,
   support: "522638136635817986",
-  owner: "336389636878368770"
+  owner: "336389636878368770",
+  permissions: []
 }
