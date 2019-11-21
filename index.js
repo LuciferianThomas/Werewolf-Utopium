c75945e630fbd03b6fa8334c6cfbd88a7f152c33
Glitch (lxfrn-helper) <none>
2019-11-21 11:25:47 +0000

    🎪🎥 Checkpoint ./index.js:4959043/1565 ./watch.json:4959043/2

--- index.js
+++ index.js
@@ -58,28 +58,45 @@ client.on('ready', async () => {
     now.alert = await alert_res.text()
     now.alert = now.alert.match(/<div class=\"title_sign3\">\s*?<table>(?:.|\s)*?<\/table>\s*?<\/div>(?:.|\s)*?<table (?:.|\s)*?<\/table>/g)
       
-    for (var i = 0; i < now.alert.length; i++)
-      now.alert[i] = now.alert[i].replace(/(\n|\r)+/g, "\n")
-          .replace(/<div s.*?>((?:.|\s)*?)<\/div>/g, "$1")
-          .replace(/<td.*?>((?:.|\s)*?)<\/td>/g, "$1\t")
-          .replace(/<th.*?>((?:.|\s)*?)<\/th>/g, "$1\t")
-          .replace(/<div><\/div>/g, "")
-          .replace(/<tr.*?>((?:.|\s)*?)<\/tr>/g, "$1\n")
-          .replace(/<br.*?>/g, "\n")
-          .replace(/<table.*?>((?:.|\s)*?)<\/table>/g, "$1")
-          .replace(/<p><!--.*?--><\/p>/g, "").replace(/<img.*?to.*?>/g, "↔")
-          .replace(/<img.*?sign_alert.png.*?>/g, "⚠").replace(/\n( |\t)*?\n/g, "\n")
-          .replace(/\n( |\t)*?\n/g, "\n").replace(/ {3,}/g, "").replace(/\n\t/g, "\n")
-          .replace(/<p.*?>((?:.|\s)*?)<\/p>/g, "$1")
-          .replace(/<div class=\"title_sign3\">((?:.|\s)*?)<\/div>/g,
-                   "===========================\n$1")
-          .replace(/<div c.*?>((?:.|\s)*?)<\/div>/g, "")
-          .replace(/⚠\n<strong>(.*?)<\/strong>/g, "⚠ $1").replace(/<!--.*?-->/g, "")
-          .replace(/<div i.*?>/g, "").replace(/<sup><\/sup>/g, "").replace(/\n /g, " ")
-          .replace(/\t\n/g, "\n").replace(/\n{2,}/g, "\n")
-            
+    for (var i = 0; i < now.alert.length; i++) {
+      let text = now.alert[i].replace(/(\n|\r)+/g, "\n")
+        .replace(/<div s.*?>((?:.|\s)*?)<\/div>/g, "$1")
+        .replace(/<td.*?>((?:.|\s)*?)<\/td>/g, "$1\t")
+        .replace(/<th.*?>((?:.|\s)*?)<\/th>/g, "$1\t")
+        .replace(/<div><\/div>/g, "")
+        .replace(/<tr.*?>((?:.|\s)*?)<\/tr>/g, "$1\n")
+        .replace(/<br.*?>/g, "\n")
+        .replace(/<table.*?>((?:.|\s)*?)<\/table>/g, "$1")
+        .replace(/<p><!--.*?--><\/p>/g, "").replace(/<img.*?to.*?>/g, "↔")
+        .replace(/<img.*?sign_alert.png.*?>/g, "⚠").replace(/\n( |\t)*?\n/g, "\n")
+        .replace(/\n( |\t)*?\n/g, "\n").replace(/ {3,}/g, "").replace(/\n\t/g, "\n")
+        .replace(/<p.*?>((?:.|\s)*?)<\/p>/g, "$1")
+        .replace(/<div class=\"title_sign3\">((?:.|\s)*?)<\/div>/g, "$1")
+        .replace(/<div c.*?>((?:.|\s)*?)<\/div>/g, "")
+        .replace(/⚠\n<strong>(.*?)<\/strong>/g, "⚠ $1").replace(/<!--.*?-->/g, "")
+        .replace(/<div i.*?>/g, "").replace(/<sup><\/sup>/g, "").replace(/\n /g, " ")
+        .replace(/\t\n/g, "\n").replace(/\n{2,}/g, "\n")
+        .trim()
       
-    console.log(now.alert[now.alert.length-1])
+      now.alert[i] = {
+        title: text.split("\n")[0],
+        timestamp: moment(text.split("\n")[1].split("This message issued  : ")[1].trim()),
+        content: text.split("\n").slice(2).join("\n")
+      }
+      
+      if (!last.alert.find(alert => alert.title == now.alert[i].title)) {
+        await client.users.get("336389636878368770").send(
+          new Discord.RichEmbed()
+            .setColor(0xEC4783)
+            .setTitle(now.alert[i].title)
+            .setURL("http://www.mtr.com.hk/alert/alert_simpletxt_title.html")
+            .setThumbnail("https://cdn.glitch.com/d7b6f4af-db94-4fb0-9341-aa45140f4d36%2FMTR.png?v=1574086190653")
+            .setDescription(now.alert[i].content)
+            .setFooter("Updated")
+            .setTimestamp(now.alert[i].timestamp)
+        )
+      }
+    }
 
     last.tsi = now.tsi
     let tsi_res = await fetch("http://www.mtr.com.hk/alert/tsi_simpletxt_title.html")
@@ -90,22 +107,22 @@ client.on('ready', async () => {
       .replace(/\<\/tr\>/g, "\n")
       .replace(/\<\/td\>/g, "\t")
 
-    return;
+    // return;
     
-    let alertDifference = diff.diffLines(last.alert, now.alert).filter(x => x.added && !x.value.includes("This message issued") && !x.value.trim().endsWith(";"))
+    // let alertDifference = diff.diffLines(last.alert, now.alert).filter(x => x.added && !x.value.includes("This message issued") && !x.value.trim().endsWith(";"))
     let tsiDifference = diff.diffLines(last.tsi, now.tsi).filter(x => x.added && !x.value.includes("This message issued") && !x.value.trim().endsWith(";"))
-    if (striptags(alertDifference.map(x => x.value.trim()).join("\n")).length) {
-      await client.users.get("336389636878368770").send(
-        new Discord.RichEmbed()
-          .setColor(0xEC4783)
-          .setTitle("Train Service Delay/Disruption Announcement Updated")
-          .setURL("http://www.mtr.com.hk/alert/alert_simpletxt_title.html")
-          .setThumbnail("https://cdn.glitch.com/d7b6f4af-db94-4fb0-9341-aa45140f4d36%2FMTR.png?v=1574086190653")
-          .setDescription(`${striptags(alertDifference.map(x => x.value.trim()).join("\n"))}`)
-          .setFooter("Updated")
-          .setTimestamp()
-      )
-    }
+    // if (striptags(alertDifference.map(x => x.value.trim()).join("\n")).length) {
+    //   await client.users.get("336389636878368770").send(
+    //     new Discord.RichEmbed()
+    //       .setColor(0xEC4783)
+    //       .setTitle("Train Service Delay/Disruption Announcement Updated")
+    //       .setURL("http://www.mtr.com.hk/alert/alert_simpletxt_title.html")
+    //       .setThumbnail("https://cdn.glitch.com/d7b6f4af-db94-4fb0-9341-aa45140f4d36%2FMTR.png?v=1574086190653")
+    //       .setDescription(`${striptags(alertDifference.map(x => x.value.trim()).join("\n"))}`)
+    //       .setFooter("Updated")
+    //       .setTimestamp()
+    //   )
+    // }
     if (striptags(tsiDifference.map(x => x.value.trim()).join("\n")).length) {
       await client.users.get("336389636878368770").send(
         new Discord.RichEmbed()
