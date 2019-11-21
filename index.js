e3c82770476976e0891b6017bb29a7bbd89d7a05
Glitch (lxfrn-helper) <none>
2019-11-21 10:38:38 +0000

    📫🎳 Checkpoint ./index.js:4959043/100 ./watch.json:4959043/14

--- index.js
+++ index.js
@@ -59,10 +59,12 @@ client.on('ready', async () => {
     now.alert = now.alert.match(/<div class=\"title_sign3\">\s*?<table>(?:.|\s)*?<\/table>\s*?<\/div>(?:.|\s)*?<table (?:.|\s)*?<\/table>/g)
     //console.log(now.alert)
     for (var i = 0; i < now.alert.length; i++)
-      now.alert[i] = now.alert[i].replace(/<div s.*?>((?:.|\s)*?)<\/div>/g, "$1")
+      console.log(now.alert[i].replace(/<div s.*?>((?:.|\s)*?)<\/div>/g, "$1")
           .replace(/<td.*?>((?:.|\s)*?)<\/td>/g, "$1\t")
-          .replace(/<th.*?>((?:.|\s)*?)<\/th>/g, "$1\t").replace(/<div><\/div>/g, "")
-          .replace(/<tr.*?>((?:.|\s)*?)<\/tr>/g, "$1\n").replace(/<br.*?>/g, "\n")
+          .replace(/<th.*?>((?:.|\s)*?)<\/th>/g, "$1\t")
+          .replace(/<div><\/div>/g, "")
+          .replace(/<tr.*?>((?:.|\s)*?)<\/tr>/g, "$1\n")
+    /*      .replace(/<br.*?>/g, "\n")
           .replace(/<table.*?>((?:.|\s)*?)<\/table>"/g, "$1")
           .replace(/<p><!--.*?--><\/p>"/g, "").replace(/<img.*?to.*?>/g, "↔")
           .replace(/<img.*?sign_alert.png.*?>/g, "⚠").replace(/\n( |\t)*?\n/g, "\n")
@@ -73,9 +75,10 @@ client.on('ready', async () => {
           .replace(/<div c.*?>((?:.|\s)*?)<\/div>/g, "")
           .replace(/⚠\n<strong>(.*?)<\/strong>/g, "⚠ $1").replace(/<!--.*?-->/g, "")
           .replace(/<div i.*?>/g, "").replace(/<sup><\/sup>/g, "").replace(/\n /g, " ")
-          .replace(/\t\n/g, "\n").replace(/\n{2,}/g, "\n");
+          .replace(/\t\n/g, "\n").replace(/\n{2,}/g, "\n") */
+                 )
       
-    console.log(now.alert)
+    //console.log(now.alert)
 
     last.tsi = now.tsi
     let tsi_res = await fetch("http://www.mtr.com.hk/alert/tsi_simpletxt_title.html")
