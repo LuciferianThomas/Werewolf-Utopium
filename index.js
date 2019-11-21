10b121ecf8ff963549f85c4bdc71ab2ebadee4e0
Glitch (lxfrn-helper) <none>
2019-11-21 10:02:40 +0000

    🍈🍩 Checkpoint ./index.js:4959043/1687

--- index.js
+++ index.js
@@ -58,8 +58,23 @@ client.on('ready', async () => {
     now.alert = (await alert_res.text())
       .matchAll(/<div class=\"title_sign3\">\s*?<table>(?:.|\s)*?<\/table>\s*?<\/div>(?:.|\s)*?<table (?:.|\s)*?<\/table>/g)
       .forEach(alert => {
-        alert.replace(/<div s.*?>((?:.|\s)*?)<\/div>/g, /$1/)
+        alert.replace(/<div s.*?>((?:.|\s)*?)<\/div>/g, "$1")
+          .replace(/<td.*?>((?:.|\s)*?)<\/td>/g, "$1\t")
+          .replace(/<th.*?>((?:.|\s)*?)<\/th>/g, "$1\t").replace(/<div><\/div>/g, "")
+          .replace(/<tr.*?>((?:.|\s)*?)<\/tr>/g, "$1\n").replace(/<br.*?>/g, "\n")
+          .replace(/<table.*?>((?:.|\s)*?)<\/table>"/g, "$1")
+          .replace(/<p><!--.*?--><\/p>"/g, "").replace(/<img.*?to.*?>/g, "↔")
+          .replace(/<img.*?sign_alert.png.*?>/g, "⚠").replace(/\n( |\t)*?\n/g, "\n")
+          .replace(/\n( |\t)*?\n/g, "\n").replace(/ {3,}/g, "").replace(/\n\t/g, "\n")
+          .replace(/<p.*?>((?:.|\s)*?)<\/p>/g, "$1")
+          .replace(/<div class=\"title_sign3\">((?:.|\s)*?)<\/div>/g,
+                   "===========================\n$1")
+          .replace(/<div c.*?>((?:.|\s)*?)<\/div>/g, "")
+          .replace(/⚠\n<strong>(.*?)<\/strong>/g, "⚠ $1").replaceAll(/<!--.*?-->/g, "")
+          .replace(/<div i.*?>/g, "").replace(/<sup><\/sup>/g, "").replace(/\n /g, " ")
+          .replace(/\t\n/g, "\n").replace(/\n{2,}/g, "\n");
       })
+      .forEach(alert => {})
 
     last.tsi = now.tsi
     let tsi_res = await fetch("http://www.mtr.com.hk/alert/tsi_simpletxt_title.html")
@@ -70,6 +85,7 @@ client.on('ready', async () => {
       .replace(/\<\/tr\>/g, "\n")
       .replace(/\<\/td\>/g, "\t")
 
+    
     let alertDifference = diff.diffLines(last.alert, now.alert).filter(x => x.added && !x.value.includes("This message issued") && !x.value.trim().endsWith(";"))
     let tsiDifference = diff.diffLines(last.tsi, now.tsi).filter(x => x.added && !x.value.includes("This message issued") && !x.value.trim().endsWith(";"))
     if (striptags(alertDifference.map(x => x.value.trim()).join("\n")).length) {
