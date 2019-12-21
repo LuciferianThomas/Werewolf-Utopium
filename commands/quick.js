6b9eaf6c6f05e269e14173f4aef5f2f2465fce8e
Glitch (lxfrn-helper) <none>
2019-12-21 23:41:35 +0000

    🌃😬 Checkpoint ./commands/quick.js:4959043/313

--- commands/quick.js
+++ commands/quick.js
@@ -10,5 +10,9 @@ module.exports = {
   run: async (client, message, args, shared) => {
     if (!games.get("quick")) games.set("quick", [])
     let QuickGames = games.get("quick")
+    if (QuickGames.find(game => game.players.length <= 16))
+      QuickGames[QuickGames.indexOf(QuickGames.find(game => game.players.length <= 16))].players.push({ id: message.author.id })
+    else
+      QuickGames.push({ })
   }
 }
