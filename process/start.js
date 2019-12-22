0a9cb1d5b1230d947e9f8b9bed7aa534fecef378
Glitch (lxfrn-helper) <none>
2019-12-22 06:09:36 +0000

    🏮🎤 Checkpoint ./process/start.js:4959043/478

---
+++ process/start.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js"),
+      moment = require("moment"),
+      db = require("quick.db")
+
+const games = new db.table("Games")
+
+const broadcast = async (client, mode = "quick", gameID) => {
+  
+}
+
+module.export = async (client, gameID) => {
+  
+}
