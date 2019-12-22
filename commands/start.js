9e97492346f07d83b21f299657ad00b57d2af96f
Glitch (lxfrn-helper) <none>
2019-12-22 02:28:35 +0000

    ⛸🎈 Checkpoint ./commands/start.js:4959043/1047

--- commands/start.js
+++ commands/start.js
@@ -9,9 +9,20 @@ module.exports = {
   name: "start",
   run: async (client, message, args, shared) => {
     let player = players.get(message.author.id)
-    if (!player.currentGame) return await message.author.send("**You are not currently in a game!**\nDo `w!quick` to join a Quick Game!")
-    let game = games.get(player.currentGame)
-    if (!game.startVotes)
+    if (!player.currentGame) 
+      return await message.author.send("**You are not currently in a game!**\nDo `w!quick` to join a Quick Game!")
     
+    let game = games.get(player.currentGame)
+    if (game.players.length < 4) 
+      return await message.author.send("**There are insufficient players to start a game!**\nInvite your friends to join the game!")
+    
+    if (!game.startVotes) games.set(`${player.currentGame}.startVotes`, 0)
+    let votes = games.get(`${player.currentGame}.startVotes`)
+    
+    for (var i = 0; i < game.players.length; i++) {
+      await client.users.get(game.players[i].id).send(`**${message.author.username}** voted to start! (${votes+1}/${game.players.length})\nDo \`w!start\` if you want the game to start.`)
+    }
+    
+    //if (votes+1 == game.players.length)
   }
 }
