13dd5e1b3faee161305bf063197cffd494fa354e
Glitch (lxfrn-helper) <none>
2019-12-22 00:04:37 +0000

    🚃🏵 Checkpoint ./commands/quick.js:4959043/520

--- commands/quick.js
+++ commands/quick.js
@@ -8,15 +8,26 @@ module.exports = {
   name: "quick",
   aliases: ["joingame", "q"],
   run: async (client, message, args, shared) => {
+    if (!games.get("count")) games.set("count", 0)
     if (!games.get("quick")) games.set("quick", [])
     let QuickGames = games.get("quick")
+    
     if (QuickGames.find(game => game.players.length <= 16))
       QuickGames[QuickGames.indexOf(QuickGames.find(game => game.players.length <= 16))].players.push({ id: message.author.id })
     else
       QuickGames.push({
+        gameID: games.add("count", 1),
         nextDay: null,
         nextNight: null,
-        roles: ["Aura Seer", "Medium", "Jailer", "Werewolf", "Doctor", "Alpha Werewolf", "Seer", Math.random() < 0.5 ? "Fool" : "Headhunter", "Bodyguard", "]
+        roles: ["Aura Seer", "Medium", "Jailer", "Werewolf", "Doctor", "Alpha Werewolf", "Seer", Math.random() < 0.5 ? "Fool" : "Headhunter",
+                "Bodyguard", "Gunner", "Werewolf Shaman", "Serial Killer", "Cursed", "Priest", "Wolf Seer", "Aura Seer"],
+        players: [{
+          id: message.author.id
+        }]
       })
+    
+    games.set("quick", QuickGames)
+    
+    message.author.send(`You have joined Game #${games}.`)
   }
 }
