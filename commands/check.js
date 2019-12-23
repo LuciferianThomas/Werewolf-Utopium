3d463afd341ed6907d47b12ceb59856a867a4674
Glitch (lxfrn-helper) <none>
2019-12-23 07:07:13 +0000

    🎻🌅 Checkpoint ./commands/check.js:4959043/863

--- commands/check.js
+++ commands/check.js
@@ -33,7 +33,12 @@ module.exports = {
       return await message.author.send("You cannot check on yourself.")
     
     if (gamePlayer.role == "Aura Seer")
-      message.author.send(`${target} ${client.users.getgame.players[]}`)
+      message.author.send(`${target} ${client.users.get(game.players[target-1].id).username} has a${
+                            ["Medium","Jailer","Alpha Werewolf","Fool","Headhunter","Gunner","Serial Killer"].includes(game.players[target-1].role) ? "n unknown" :
+                            ["Werewolf","Shaman Werewolf","Wolf Seer"].includes(game.players[target-1].role) ? "n evil" : " good"
+                          } aura`)
+    else
+      message.author.send(`${target} ${client.users.get(game.players[target-1].id).username} is a${["A","E","I","O","U"].includes(game.players[target-1].role[0]) ? "n" : ""} ${game.players[target-1].role}`)
     
     QuickGames[index] = game
     
