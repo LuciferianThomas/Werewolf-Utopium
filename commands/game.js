df2c4bffae0ae0e2d266ce01b0007d4a217518cf
Glitch (wwo-utopium) <none>
2019-12-27 06:28:03 +0000

    🌘⏳ Checkpoint ./commands/game.js:4959043/959 ./watch.json:4959043/4

--- commands/game.js
+++ commands/game.js
@@ -5,7 +5,8 @@ const Discord = require("discord.js"),
 const games = new db.table("Games"),
       players = new db.table("Players")
 
-const fn = require('/app/util/fn')
+const fn = require('/app/util/fn'),
+      roles = require('/app/util/roles')
 
 module.exports = {
   name: "game",
@@ -26,10 +27,15 @@ module.exports = {
                   game.currentPhase == -1 ? game.players.map(p => client.users.get(p.id).username).join('\n') :
                   game.players
                     .map(p => 
-                           `${p.alive ? "" : "*"}${p.number} ${client.users.get(p.id).username}${p.roleRevealed ? ` (${p.role})` : ""}${p.alive ? "" : "*"}`)
+                           `${p.alive ? "" : "*"}${p.number} ${client.users.get(p.id).username}${
+                             p.id == message.author.id ? ` ${client.emojis.find(e => e.name == p.role.replace(/ /g, "_"))}` : 
+                             p.roleRevealed ? ` ${client.emojis.find(e => e.name == p.role.replace(/ /g, "_"))}` : 
+                             roles[p.role].team == "Werewolf" ? ` ${client.emojis.find(e => e.name == "Fellow_Werewolf")}` : ""}${p.alive ? "" : "*"}`)
                     .join('\n')
                  )
-        .addField(`Roles`, game.roles.sort((a,b) => {if (a > b) return 1; if (a < b) return -1}).join(', '))
+        .addField(`Roles`, game.roles.sort((a,b) => {if (a > b) return 1; if (a < b) return -1})
+                            .map(r => `${client.emojis.find(e => e.name == r.replace(/ /g, "_"))} ${r}`)
+                            .join('\n'))
     )
   }
 }
