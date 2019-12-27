f1e2dc5141c0b0d0b6d0e55caf00067a3e5b7bfa
Glitch (wwo-utopium) <none>
2019-12-27 06:52:03 +0000

    🏚💧 Checkpoint ./commands/role.js:4959043/845

--- commands/role.js
+++ commands/role.js
@@ -5,8 +5,10 @@ const roles = require('/app/util/roles')
 module.exports = {
   name: "role",
   run: async (client, message, args, shared) => {
-    let targetRole = args.forEach(x => x[0] = x[0].toUpperCase()).join(' ')
-    let role = roles[targetRole]
-    if (!role) role = Object.values(roles).find(r => r.aliases.includes(targetRole.toLowerCase()))
+    if (!args.length) return await message.author.send("You did not specify a role.")
+    
+    let targetRole = args.join(' ')
+    let role = roles[Object.keys(roles).find(r => r.toLowerCase() == targetRole.toLowerCase())] || Object.values(roles).find(r => r.aliases.includes(targetRole.toLowerCase()))
+    if (!role) return await message.author.send("Invalid ")
   }
 }
