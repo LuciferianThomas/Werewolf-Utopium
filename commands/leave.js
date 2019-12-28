8e85fb7f25cc23fa8b21fad18bdc0ca39a356c81
Glitch (wwo-utopium) <none>
2019-12-28 23:56:44 +0000

    👶🐉 Checkpoint ./commands/leave.js:4959043/1047

--- commands/leave.js
+++ commands/leave.js
@@ -19,8 +19,20 @@ module.exports = {
     if (game.currentPhase == -1) {
       let m = await message.author.send("Are you sure you want to leave the game?")
       m.react(client.emojis.find(e => e.name == "green_tick"))
-      let reactions = await m.awaitReactions()
+      let reactions = await m.awaitReactions(
+        (r, u) => r.emoji.id == client.emojis.find(e => e.name == "green_tick").id &&
+                  u.id == message.author.id,
+        { max: 1, time: 5000, errors: ["time"] }
+      ).catch(() => {})
+      if (!reactions) return await message.author.send("Prompt cancelled.")
+      game = games.get("quick").find(g => g.id == player.currentGame)
       game.players.splice(game.players.indexOf(gamePlayer), 1)
     }
+    QuickGames = games.get("quick")
+    QuickGames[QuickGames.indexOf(QuickGames.find(g => g.id == game.id))] = game
+    
+    games.set("quick", QuickGames)
+    
+    message.author.send(`You left Game #${game.id}.`)
   }
 }
