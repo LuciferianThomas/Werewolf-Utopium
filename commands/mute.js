90c6fdb2b0a737bc11dd9e47557302b558d4ba7b
Glitch (wwo-utopium) <none>
2020-01-16 09:47:41 +0000

    💕💴 Checkpoint ./index.js:4959043/4823 ./watch.json:4959043/6 ./commands/mute.js:4959043/4153

---
+++ commands/mute.js
@@ -0,0 +1,56 @@
+const Discord = require("discord.js"),
+      moment = require("moment"),
+      db = require("quick.db")
+
+const games = new db.table("Games"),
+      players = new db.table("Players")
+
+const fn = require('/app/util/fn')
+
+module.exports = {
+  name: "mute",
+  run: async (client, message, args, shared) => {
+    let player = players.get(message.author.id)
+    if (!player.currentGame) 
+      return await message.author.send("**You are not currently in a game!**\nDo `w!quick` to join a Quick Game!")
+    
+    let QuickGames = games.get("quick"),
+        game = QuickGames.find(g => g.gameID == player.currentGame),
+        index = QuickGames.indexOf(game),
+        gamePlayer = game.players.find(player => player.id == message.author.id)
+    
+    if (gamePlayer.role !== "Grumpy Grandma")
+      return await message.author.send("You do not have the abilities to mute a player.")
+    if (!gamePlayer.alive)
+      return await message.author.send("You are dead. You can no longer mute a player.")
+    if (gamePlayer.jailed)
+      return await message.author.send("You are currently jailed and cannot use your abilities.")
+    
+    if (game.currentPhase % 3 != 0)
+      return await message.author.send("You can only select to mute a player at night.")
+    
+    let target = parseInt(args[0])
+    if (isNaN(target) || target > game.players.length || target < 1)
+      return await message.author.send("Invalid target.")
+    
+    let targetPlayer = game.players[target-1]
+    if (!targetPlayer.alive)
+      return await message.author.send("You cannot mute an dead player.")
+    if (target == gamePlayer.number)
+      return await message.author.send("You cannot mute yourself.")
+    
+    gamePlayer.mute = target
+    
+    message.author.send(
+      `${
+        fn.getEmoji(client, "Grumpy Grandma Mute")
+      } You selected **${target} ${
+        client.users.get(game.players[target - 1].id).username
+      }** to be muted the next day.`
+    )
+    
+    QuickGames[index] = game
+    
+    games.set("quick", QuickGames)
+  }
+}
--- index.js
+++ index.js
@@ -61,31 +61,13 @@ client.on('ready', async () => {
       if (game.currentPhase < 999)
         for (let pl = 0; pl < game.players.length; pl++) {
           if (game.currentPhase == -1) {
-            if (!fn.getUser(client, game.players[pl].id)) {
+            if (!fn.getUser(client, game.players[pl].id) && moment(game.players[pl].lastAction).add(2, 'm') <= moment()) {
+              if (!fn.getUser(client, game.players[pl].id))
+                fn.getUser(client, game.players[pl].id).send(`You are removed from Game #${game.gameID} for inactivity.`)
+              
               players.set(`${game.players[pl].id}.currentGame`, 0)
               fn.broadcastTo(
-                client,
-                game.players,
-                new Discord.RichEmbed()
-                  .setAuthor(
-                    `**${fn.getUser(client, game.players[pl].id).username}** left the game.`,
-                    fn.getUser(client, game.players[pl].id).displayAvatarURL
-                  )
-                  .addField(
-                    `Players [${game.players.length}]`,
-                    game.players
-                      .map(p => fn.getUser(client, p.id).username)
-                      .join("\n")
-                  )
-              )
-              game.players.splice(pl--, 1)
-            }
-            else if (moment(game.players[pl].lastAction).add(2, 'm') <= moment()) {
-              fn.getUser(client, game.players[pl].id).send(`You are removed from Game #${game.gameID} for inactivity.`)
-              players.set(`${game.players[pl].id}.currentGame`, 0)
-              fn.broadcastTo(
-                client,
-                game.players,
+                client, game.players.filter(p => p.id == game.players[pl].id),
                 new Discord.RichEmbed()
                   .setAuthor(
                     `**${fn.getUser(client, game.players[pl].id).username}** left the game.`,
@@ -111,28 +93,8 @@ client.on('ready', async () => {
               )
             }
           } else {
-            if (!fn.getUser(client, game.players[pl].id)) {
-              game.players[pl].alive = false
-              game.players[pl].left = true
-              game.players[pl].suicide = true
-              if (game.config.deathReveal) game.players[pl].roleRevealed = game.players[pl].role
-              players.add(`${game.players[pl].id}.suicides`, 1)
-              players.set(`${game.players[pl].id}.currentGame`, 0)
-              
-              fn.broadcastTo(
-                client, game.players.filter(p => !p.left),
-                `**${game.players[pl].number} ${fn.getUser(
-                  client,
-                  game.players[pl].id
-                )}${
-                  game.config.deathReveal
-                    ? ` ${fn.getEmoji(client, game.players[pl].role)}`
-                    : ""
-                }** suicided.`
-              )
-            }
             if (!game.players[pl].alive || game.players[pl].left) continue;
-            if (moment(game.players[pl].lastAction).add(2, 'm') <= moment()) {
+            if (!fn.getUser(client, game.players[pl].id) && moment(game.players[pl].lastAction).add(2, 'm') <= moment()) {
               game.players[pl].alive = false
               game.players[pl].left = true
               game.players[pl].suicide = true
@@ -140,9 +102,10 @@ client.on('ready', async () => {
               players.add(`${game.players[pl].id}.suicides`, 1)
               players.set(`${game.players[pl].id}.currentGame`, 0)
 
-              fn.getUser(client, game.players[pl].id).send(`You were removed from Game #${game.gameID} for inactivity.`)
+              if (!fn.getUser(client, game.players[pl].id))
+                fn.getUser(client, game.players[pl].id).send(`You were removed from Game #${game.gameID} for inactivity.`)
               fn.broadcastTo(
-                client, game.players.filter(p => !p.left),
+                client, game.players.filter(p => !p.left && p.id == game.players[pl].id),
                 `**${game.players[pl].number} ${fn.getUser(
                   client,
                   game.players[pl].id
@@ -154,14 +117,15 @@ client.on('ready', async () => {
               )
             } else if (moment(game.players[pl].lastAction).add(1.5, 'm') <= moment() && !game.players[pl].prompted) {
               game.players[pl].prompted = true
-              fn.getUser(client, game.players[pl].id).send(
-                "**You have been inactive for 1.5 minutes.**\n" +
-                "Please respond `w!` within 30 seconds to show your activity.\n" +
-                "You will be considered as suicide if you fail to do so."
-              )
-            }
+              new Discord.RichEmbed()
+                .setTitle("❗ You have been inactive for 1.5 minutes.")
+                .setDescription(
+                  "Please respond `w!` within 30 seconds to show your activity.\n" +
+                  "You will be considered as suicided if you fail to do so."
+                )
           }
         }
+      }
       
       if (game.currentPhase === 999) {
         fn.broadcastTo(
