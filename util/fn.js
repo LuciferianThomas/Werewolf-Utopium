625447beb0ce5298fd8af5fcfc894a7c18e9fb4b
Glitch (wwo-utopium) <none>
2020-01-17 05:28:48 +0000

    🌿🚐 Checkpoint ./util/fn.js:4959043/2342

--- util/fn.js
+++ util/fn.js
@@ -107,33 +107,31 @@ let modCaseEmbed = (client, thisCase) => {
 }
 
 let paginator = async (author, msg, embeds, pageNow) => {
-  if (pageNow != 0) {
+  if (addReactions) {
     await msg.react("⏪")
     await msg.react("◀")
-  }
-  if (pageNow != embeds.length-1) {
     await msg.react("▶")
     await msg.react("⏩")
   }
-  let reaction = await msg.awaitReactions((reaction, user) => user.id == author && ["◀","▶","⏪","⏩"].includes(reaction.emoji.name), {time: 30*1000, max:1, errors: ['time']}).catch(() => msg.clearReactions().catch(() => {}))
-  if (!reaction.size) return undefined
+  let reaction = await msg.awaitReactions((reaction, user) => user.id == author && ["◀","▶","⏪","⏩"].includes(reaction.emoji.name), {time: 30*1000, max:1, errors: ['time']}).catch(() => {})
+  if (!reaction) return msg.clearReactions().catch(() => {})
   reaction = reaction.first()
   if (reaction.emoji.name == "◀") {
-    let m = await msg.channel.send(embeds[Math.max(pageNow-1, 0)])
-    msg.delete()
-    paginator(author, m, embeds, Math.max(pageNow-1, 0))
+    await reaction.remove(author)
+    let m = await msg.edit(embeds[Math.max(pageNow-1, 0)])
+    paginator(author, m, embeds, Math.max(pageNow-1, 0), false)
   } else if (reaction.emoji.name == "▶") {
-    let m = await msg.channel.send(embeds[Math.min(pageNow+1, embeds.length-1)])
-    msg.delete()
-    paginator(author, m, embeds, Math.min(pageNow+1, embeds.length-1))
+    await reaction.remove(author)
+    let m = await msg.edit(embeds[Math.min(pageNow+1, embeds.length-1)])
+    paginator(author, m, embeds, Math.min(pageNow+1, embeds.length-1), false)
   } else if (reaction.emoji.name == "⏪") {
-    let m = await msg.channel.send(embeds[0])
-    msg.delete()
-    paginator(author, m, embeds, 0)
+    await reaction.remove(author)
+    let m = await msg.edit(embeds[0])
+    paginator(author, m, embeds, 0, false)
   } else if (reaction.emoji.name == "⏩") {
-    let m = await msg.channel.send(embeds[embeds.length-1])
-    msg.delete()
-    paginator(author, m, embeds, embeds.length-1)
+    await reaction.remove(author)
+    let m = await msg.edit(embeds[embeds.length-1])
+    paginator(author, m, embeds, embeds.length-1, false)
   }
 }
 
