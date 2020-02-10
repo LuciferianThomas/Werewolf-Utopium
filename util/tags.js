b2c348db25f503517c74ae5339f1e3cba616fc3a
Glitch (wwo-utopium) <none>
2020-02-10 16:05:33 +0000

    🌦🖐 Checkpoint ./util/roles.js:4959043/787 ./util/tags.js:4959043/516

---
+++ util/tags.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ROLES: {
+    0x0001: REGULAR VILLAGER    0x0002: STRONG VILLAGER
+  * 0x0004: WEREWOLF            0x0008: SOLO VOTING
+  * 0x0010: SOLO KILLING        0x0020: SEEN AS VILLAGER
+  * 0x0040: SEEN AS WEREWOLF    0x0080: 
+  * 0x0100:                     0x0200:
+  * 0x0400:                     0x0800: WWC ROLES
+  * 0x1000: AVAILABLE           0x2000: TO BE TESTED
+  * 0X4000: UNAVAILABLE         0x8000: WWOWC ROLES
+  }
+}
