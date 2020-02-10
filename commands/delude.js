2c7bdabb2072fbe936b3095037920dd4c6978fbe
Glitch (wwo-utopium) <none>
2020-02-10 06:03:19 +0000

    🐴☔️ Checkpoint ./commands/illusion.js:4959043/2759 ./commands/delude.js:4959043/216 ./commands/avenge.js:4959043/48

--- commands/delude.js
+++ commands/delude.js
@@ -10,8 +10,8 @@ const fn = require('/app/util/fn'),
       roles = require("/app/util/roles")
 
 module.exports = {
-  name: "avenge",
-  aliases: ["tag"],
+  name: "delude",
+  aliases: ["illusion", "illu"],
   run: async (client, message, args, shared) => {
     let player = players.get(message.author.id)
     if (!player.currentGame) 
@@ -22,14 +22,14 @@ module.exports = {
         index = QuickGames.indexOf(game),
         gamePlayer = game.players.find(player => player.id == message.author.id)
     
-    if (!["Junior Werewolf","Avenger"].includes(gamePlayer.role))
+    if (gamePlayer.role !== "Illusionist")
       return await message.author.send("You do not have the abilities to avenge on a player.")
     if (!gamePlayer.alive)
       return await message.author.send("You are dead. You can no longer avenge on a player.")
     if (gamePlayer.jailed)
       return await message.author.send("You are currently jailed and you cannot use your abilities!")
-    if (game.currentPhase == 0 && gamePlayer.role == "Avenger")
-      return await message.author.send("You cannot select a player to be avenged on right now!")
+    if (game.currentPhase % 3 != 0)
+      return await message.author.send("You can only select a player to be deluded at night!")
     
     let target = parseInt(args[0])
     if (isNaN(target) || target > game.players.length || target < 1)
@@ -41,14 +41,11 @@ module.exports = {
     if (roles[gamePlayer.role].team == roles[targetPlayer.role].team == "Werewolves")
       return await message.author.send("You cannot avenge on your fellow werewolves!")
     
-    if (gamePlayer.role == "Avenger" && !gamePlayer.sect && targetPlayer.role == "President")
-      return await message.author.send("You cannot avenge on the President!")
-    
     gamePlayer.avenge = targetPlayer.number
     
     message.author.send(
-      `${fn.getEmoji(client, gamePlayer.role == "Avenger" ? "Avenger Select" : "Junior Werewolf Select")
-      } You selected **${target} ${nicknames.get(targetPlayer.id)}** to be avenged on when you die.`
+      `${fn.getEmoji(client, "Illusionist Delude")
+      } You selected to delude the identity of **${target} ${nicknames.get(targetPlayer.id)}**.`
     )
     
     QuickGames[index] = game
