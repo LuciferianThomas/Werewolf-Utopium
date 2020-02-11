3fb2c9e9030e152f81d099c326a73d35ea10d885
Glitch (wwo-utopium) <none>
2020-02-11 03:19:55 +0000

    📊🕹 Checkpoint ./process/game.js:4959043/198

--- process/game.js
+++ process/game.js
@@ -67,6 +67,7 @@ module.exports = (client) => {
                   `**${lynched} ${nicknames.get(lynchedPlayer.id)}${
                     game.config.deathReveal ? ` ${fn.getEmoji(client, lynchedPlayer.role)}` : ""}** was lynched by the village.`
                 )
+                lynchedPlayer.killedBy = -1
 
                 game = fn.death(client, game, lynchedPlayer.number)
 
