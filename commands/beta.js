97df36b532dd189d36d5d5d375ee593468ec176d
Glitch (wwo-utopium) <none>
2020-02-12 07:41:31 +0000

    👔🛢 Checkpoint ./commands/beta.js:4959043/967 ./watch.json:4959043/10

--- commands/beta.js
+++ commands/beta.js
@@ -10,13 +10,13 @@ module.exports = {
     let input = moment(args.join(' ')).utcOffset(8)
     if (input == "Invalid date")
       return await message.channel.send("You inputted an invalid date. Please try again.")
-    let time = input.format("HH:mm [HKT]"),
+    let time = input.format("HH:mm"),
         date = input.format("MMM D, YYYY (ddd)")
     
     let embed = new Discord.RichEmbed()
       .setColor(0xe4b400)
       .setTitle("βTesting Session")
-      .setDescription(`${message.author} will be hosting a βTesting Session at [${time}](https://www.thetimezoneconverter.com/?t=${time}&tz=GMT%2B8&) on ${date}.`)
+      .setDescription(`${message.author} will be hosting a βTesting Session at [${time} HKT](https://www.thetimezoneconverter.com/?t=${time}&tz=Hong%20Kong&) on ${date}.`)
       .setFooter("React to this message to show your availability.")
     
     let m = await message.channel.send("Please confirm if this is correct.", embed)
@@ -32,9 +32,17 @@ module.exports = {
         .setTitle("Prompt cancelled.")
     ).then(m => m.clearReactions().catch(() => {}))
     
-    let βTester = fn.getRole(message.guild, "βTester")
+    let βTester = fn.getRole(message.guild, "βTester"),
+        βtest_announcements = message.guild.channels.find(c => c.name == "βtest-announcements")
     await βTester.setMentionable(true, "βTest Announcement").catch(() => {})
-    message.guild.channels.get("676642370954985501").send(`${fn.getRole(message.guild, "βTester")}`,)
+    βtest_announcements.send(`${βTester}`, embed)
     await βTester.setMentionable(false).catch(() => {})
+    
+    await m.edit(
+      new Discord.RichEmbed()
+        .setColor("GREEN")
+        .setTitle(`Announcement has been sent to ${βtest_announcements}.`)
+    )
+    await m.clearReactions().catch(() => {})
   }
 }
