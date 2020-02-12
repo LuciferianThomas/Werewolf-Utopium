2280cd4cd55ff9a628b3fbbff2b2aff004c89c78
Glitch (wwo-utopium) <none>
2020-02-12 06:54:32 +0000

    🍚🏟 Checkpoint ./commands/beta.js:4959043/298 ./util/fn.js:4959043/7817

--- commands/beta.js
+++ commands/beta.js
@@ -1,8 +1,11 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js'),
+      fn = require('')
 
 module.exports = {
   name: "beta",
   run: async (client, message, args, shared) => {
-    
+    let time = fn.time(args.join(' '))
+    if (time == "Invalid date")
+      return await message.channel.send("")
   }
 }
