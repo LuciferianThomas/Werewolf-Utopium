6cc94849dac3af8b67437075d4614ad4866173cd
Glitch (werewolf-utopium-staff) <none>
2020-03-09 02:35:35 +0000

    🌠😬 Checkpoint ./commands/now.js:4959043/309 ./watch.json:4959043/4

--- commands/now.js
+++ commands/now.js
@@ -6,15 +6,15 @@ module.exports = {
   name: "now",
   run: async (client, message, args, shared) => {
     let tzInput = args.join(' ')
-    let tz = tzInput.match(/(?:GMT|UTC)(.*?)/g)
+    let tz = parseInt(tzInput)
     
     await message.channel.send(
       new Discord.RichEmbed()
         .setTitle(`Current Time`)
         .setDescription(
           `**Hong Kong Time**: ${moment().utcOffset(8).format("HH:mm [on] MMM D, YYYY (ddd)")}\n` +
-          `**Greenwich Mean Time**: ${moment().format("HH:mm [on] MMM D, YYYY (ddd)")}\n` // +
-          // `**Your Time**: ${moment().format("HH:mm [on] MMM D, YYYY (ddd)")}\n`
+          `**Greenwich Mean Time**: ${moment().format("HH:mm [on] MMM D, YYYY (ddd)")}\n` +
+          (!isNaN(tz) ? `**Your Time**: ${moment().utcOffset(tz).format("HH:mm [on] MMM D, YYYY (ddd)")}\n` : "")
         )
     )
   }
