bc7c484f263c03246a4dbc3bf6ed6f06b77e50f8
Glitch (werewolf-utopium-staff) <none>
2020-03-09 02:58:57 +0000

    🚩🌎 Checkpoint ./commands/now.js:4959043/742 ./watch.json:4959043/10

--- commands/now.js
+++ commands/now.js
@@ -6,17 +6,6 @@ module.exports = {
   name: "now",
   run: async (client, message, args, shared) => {
     let tzInput = args.join(' ')
-    let tz = parseInt(tzInput)
-    if (!isNaN(tz)) {
-      if (tzInput.match(/\+?.*?([.,](5|50)|:30)/)) tz += .5
-      if (tzInput.match(/\-.*?([.,](5|50)|:30)/)) tz -= .5
-      if (tzInput.match(/\+?.*?([.,](25)|:15)/)) tz += .25
-      if (tzInput.match(/\-.*?([.,](25)|:15)/)) tz -= .25
-      if (tzInput.match(/\+?.*?([.,](75)|:45)/)) tz += .75
-      if (tzInput.match(/\-.*?([.,](75)|:45)/)) tz -= .75
-      tz *= 60
-      console.log(tzInput.match(/\-.*?([.,](5|50)|:30)/))
-    }
     
     await message.channel.send(
       new Discord.RichEmbed()
@@ -24,7 +13,7 @@ module.exports = {
         .setDescription(
           `**Hong Kong Time**: ${moment().utcOffset(8).format("HH:mm [on] MMM D, YYYY (ddd)")}\n` +
           `**Greenwich Mean Time**: ${moment().format("HH:mm [on] MMM D, YYYY (ddd)")}\n` +
-          (!isNaN(tz) && tz <= 14 && tz >= -12 ? `**Your Time**: ${moment().utcOffset(tz).format("HH:mm [on] MMM D, YYYY (ddd)")}\n` : "")
+          (tzInput ? `**Your Time**: ${moment().utcOffset(tzInput).format("HH:mm [on] MMM D, YYYY (ddd)")}\n` : "")
         )
     )
   }
