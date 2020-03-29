aba6da833658c6d0e522c94c6331ee608efa3ae0
Glitch (werewolf-utopium-staff) <none>
2020-03-29 18:16:54 +0000

    🌔😺 Checkpoint ./commands/platers.js:2449550/124 ./watch.json:2449550/2

--- commands/platers.js
+++ commands/platers.js
@@ -5,6 +5,6 @@ const Discord = require("discord.js"),
 module.exports = {
   name: "now",
   run: async (client, message, args, shared) => {
-    message.channel.send("")
+    message.channel.send("https://cdn.glitch.com/523fbde2-d515-4952-9b60-3526c94a58e4%2F229d59eb-907e-4b49-a40c-653154db392a.image.png?v=1585504929370")
   }
 }
