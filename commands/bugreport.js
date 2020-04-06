a8e7da7c0e5172b1f498d94d5d5f6fa21fb1db50
Glitch (werewolf-utopium-staff) <none>
2020-04-06 12:48:26 +0000

    🍤🐟 Checkpoint ./package.json:4959043/30 ./watch.json:4959043/8 ./commands/bugreport.js:4959043/26

--- commands/bugreport.js
+++ commands/bugreport.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js'),
       moment = require('moment'),
       fn = require('/app/util/fn')
-//       db = require('quick.db')
+      // db = require('quick.db')
 
 // const data = new db.table("data")
 
