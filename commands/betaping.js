28e3db30b7515d932e271982081a46e0ec5150a3
Glitch (werewolf-utopium-staff) <none>
2020-04-09 00:50:07 +0000

    🏍💎 Checkpoint ./commands/betaping.js:2449550/603 ./watch.json:2449550/485

--- commands/betaping.js
+++ commands/betaping.js
@@ -11,13 +11,16 @@ module.exports = {
   run: async (client, message, args, shared) => {
     message.delete().catch(()=>{})
     if(!args[0]) return message.channel.send("Please specify an announcement message!")
-    let x = client.guilds.get(channels.cache.get("676642370954985501").messages.cache.get(args[0])
+    let x = await client.guilds.cache.get("522638136635817986").channels.cache.get("676642370954985501").messages.fetch(args[0])
     if(!x) return await message.channel.send("Unable to find that announcement")
-    let embed = new Discord.MessageEmbed().setTitle("Beta Test Pings")
+    let embed = new Discord.MessageEmbed().setTitle("Beta Test Pings").setDescription("").setColor(0xE4B400)
+    let reactions = []
     x.reactions.cache.forEach(r => {
       if(!r.me) return
-      embed.description += `${r.emoji} - ${r.count} people`
+      reactions.push(r.emoji)
+      embed.description += `${r.emoji} - ${r.count} people\n`
     })
-    message.author.send(embed)
+    let m = await message.channel.send(embed)
+    await reactions.forEach(e => m.react(e))
   }
 }
