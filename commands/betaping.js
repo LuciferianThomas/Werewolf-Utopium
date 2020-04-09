9a8377aa2078f384cb863f926f782f3499d9df51
Glitch (werewolf-utopium-staff) <none>
2020-04-09 00:38:07 +0000

    🚧👞 Checkpoint ./commands/betaping.js:2449550/828 ./watch.json:2449550/14

--- commands/betaping.js
+++ commands/betaping.js
@@ -9,16 +9,15 @@ const config = require('/app/util/config'),
 module.exports = {
   name: "betaping",
   run: async (client, message, args, shared) => {
-    message.delete()
+    message.delete().catch(()=>{})
     if(!args[0]) return message.channel.send("Please specify an announcement message!")
-    let x = client.channels.cache.get("676642370954985501").messages.fetch(args[0])
-    let g = fn.getEmoji(client, "green_tick")
-    let b = fn.getEmoji(client, "gray_tick")
-    let r = fn.getEmoji(client, "red_tick")
-    let green = x.reactions.cache.get(g.id)
-    let gray = x.reactions.cache.get(g.id)
-    let red = x.reactions.cache.get(g.id)
+    let x = client.guilds.get(channels.cache.get("676642370954985501").messages.cache.get(args[0])
+    if(!x) return await message.channel.send("Unable to find that announcement")
     let embed = new Discord.MessageEmbed().setTitle("Beta Test Pings")
-    .setDescription("")
+    x.reactions.cache.forEach(r => {
+      if(!r.me) return
+      embed.description += `${r.emoji} - ${r.count} people`
+    })
+    message.author.send(embed)
   }
 }
