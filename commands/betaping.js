04fd98976f5a5c8438dc6f3e7342c2ab4285d8cd
Glitch (werewolf-utopium-staff) <none>
2020-04-09 01:13:07 +0000

    🙉☺️ Checkpoint ./commands/betaping.js:2449550/1363 ./watch.json:2449550/8

--- commands/betaping.js
+++ commands/betaping.js
@@ -15,15 +15,16 @@ module.exports = {
     if(!x) return await message.channel.send("Unable to find that announcement")
     let embed = new Discord.MessageEmbed().setTitle("βTesting Session Pings").setDescription("").setColor(0xE4B400)
     x.reactions.cache.forEach(r => {
+      console.log(r.users.cache.map(u => `<@${u.id}>`))
       if(!r.me) return
       embed.description += `${r.emoji} - ${r.count} people\n`
     })
     embed.description += `\n\nPlease select Green or Gray to ping those people, or Red to cancel`
     let m = await message.channel.send(embed)
-    await m.react(client.emojis.cache.find(e => e.name === "green_tick"))
-    await m.react(client.emojis.cache.find(e => e.name === "gray_tick"))
-    await m.react(client.emojis.cache.find(e => e.name === "red_tick"))
-    let reactions = await message.channel.awaitReactions(
+    await m.react(fn.getEmoji(client, "green_tick"))
+    await m.react(fn.getEmoji(client, "gray_tick"))
+    await m.react(fn.getEmoji(client, "red_tick"))
+    let reactions = await m.awaitReactions(
       (r, u) =>
       (r.emoji.id == fn.getEmoji(client, "green_tick").id ||
        r.emoji.id == fn.getEmoji(client, "gray_tick").id ||
@@ -31,17 +32,20 @@ module.exports = {
       u.id == message.author.id,
       { time: 30*1000, max: 1, errors: ['time'] }
     ).catch(() => {})
-    if (!reactions || reactions.first().emoji.id == client.emojis.cache.find(e => e.name === "red_tick").id)
+    if (!reactions || reactions.first().emoji.id == fn.getEmoji(client, "red_tick").id)
+      m.reactions.removeAll().catch(() => {})
       m.edit(
         new Discord.MessageEmbed()
         .setColor("RED")
         .setTitle("Pinging canceled.")
       )
     let reaction = reactions.first().emoji
+    m.reactions.removeAll().catch(() => {})
     let pings = ""
-    if (reaction.id == fn.getEmoji(client, "green_tick").id) {
-      
-    }
+    x.reactions.cache.forEach(r => {
+        if(r.emoji === reaction) pings = r.users.cache.map(u => `<@${u.id}>`)
+    })
+    m.edit(new Discord.MessageEmbed().setTitle("βTesting Session Pings").setDescription(`Please confirm you would like to ping these people:\n\n${pings}`).setColor(0xE4B400))
     
     //client.guilds.cache.get("522638136635817986").channels.cache.get("676642370954985501").send("")
   }
