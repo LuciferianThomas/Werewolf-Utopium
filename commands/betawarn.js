92c6db259a87d1ade42a82c66be30535afb365b3
Glitch (werewolf-utopium-staff) <none>
2020-04-14 16:21:31 +0000

    💘😀 Checkpoint ./commands/betawarn.js:2449550/2268

--- commands/betawarn.js
+++ commands/betawarn.js
@@ -96,51 +96,5 @@ module.exports = {
     
     for (var member of pingMembers)
       member.roles.remove(warnRole)
-    
-    // let reactions = betamsg.reactions.cache.filter(r => ["green_tick","gray_tick"].includes(r.emoji.name))
-    // let users = reactions.map(r => r.users.fetch())
-    // for (var user of users) await user
-    // console.log(users.map(u => u.map(u => u.id)).flat(Infinity))
-    // console.log(reactions.map(r => r.users.cache.map(u => u.id)).flat(Infinity).filter(u => u.id !== client.user.id))
-    // let embed = new Discord.MessageEmbed()
-    //   .setTitle("βTesting Session Pings")
-    //   .setDescription("")
-    //   .setColor(0xe4b400)
-    // betamsg.reactions.cache.forEach(r => {
-    //   if(!r.me) return
-    //   embed.description += `${r.emoji} - ${r.count - 1} ${(r.count - 1) === 1 ? "person" : "people"}\n`
-    // })
-    // embed.description += `\n\nPlease confirm if these are the people who you want to `
-    // let m = await message.channel.send(embed)
-    // await m.react(fn.getEmoji(client, "green_tick"))
-    // await m.react(fn.getEmoji(client, "gray_tick"))
-    // await m.react(fn.getEmoji(client, "red_tick"))
-    // let reactions = await m.awaitReactions(
-    //   (r, u) =>
-    //   (r.emoji.id == fn.getEmoji(client, "green_tick").id ||
-    //    r.emoji.id == fn.getEmoji(client, "gray_tick").id ||
-    //    r.emoji.id == fn.getEmoji(client, "red_tick").id) &&
-    //   u.id == message.author.id,
-    //   { time: 30*1000, max: 1, errors: ['time'] }
-    // ).catch(() => {})
-    // m.reactions.removeAll().catch(() => {})
-    // if (!reactions || reactions.first().emoji.id == fn.getEmoji(client, "red_tick").id){
-    //   return m.edit(
-    //     new Discord.MessageEmbed()
-    //     .setColor("RED")
-    //     .setTitle("Pinging canceled.")
-    //   )
-    // }
-    // let reaction = reactions.first().emoji
-    // let pings = ""
-    // x.reactions.cache.forEach(r => {
-    //   let u = r.users.fetch().then(u => {
-    //   console.log(u)
-    //   if(r.emoji === reaction) u.forEach(user => pings += `<@${user.id}>\n`)
-    //   })
-    // })
-    // m.edit(new Discord.MessageEmbed().setTitle("βTesting Session Pings").setDescription(`Please confirm you would like to ping these people:\n\n${pings}`).setColor(0xE4B400))
-    
-    //client.guilds.cache.get("522638136635817986").channels.cache.get("676642370954985501").send("")
   }
 }
